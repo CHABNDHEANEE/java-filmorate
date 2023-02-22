39ea8b5e7fe4b7b53baf83f16d5f0837021754aa
Daniil <rusbear060@gmail.com>
2023-02-22 09:02:37 +0300

    add user controller tests

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.UserListException;
 import ru.yandex.practicum.filmorate.exception.WrongUserInputException;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -13,11 +14,11 @@ import java.util.List;
 @RestController
 @Slf4j
 public class UserController {
-    private HashMap<Integer, User> users;
+    private HashMap<Integer, User> users = new HashMap<>();
 
     @PostMapping("/user")
     public User addUser(@RequestBody User user) {
-        checkUser(user);
+        user = checkUser(user);
         users.put(user.getId(), user);
         log.info("User added");
         return user;
@@ -25,7 +26,7 @@ public class UserController {
 
     @PutMapping("/user")
     public User updateUser(@RequestBody User user) {
-        checkUser(user);
+        user = checkUser(user);
         users.put(user.getId(), user);
         log.info("User updated");
         return user;
@@ -33,11 +34,12 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
+        checkUsersListExistence();
         log.info("All users printed");
         return new ArrayList<>(users.values());
     }
 
-    private void checkUser(User user) {
+    private User checkUser(User user) {
         if (user.getEmail().isBlank()) {
             log.info("Blank email err");
             throw new WrongUserInputException("Email can't be blank!");
@@ -54,5 +56,22 @@ public class UserController {
             log.info("Wrong bd err");
             throw new WrongUserInputException("You have a time machine! Don't ya?");
         }
+
+        if (user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        return user;
+    }
+
+    private void checkUsersListExistence() {
+        if (users.isEmpty()) {
+            log.info("User list empty error");
+            throw new UserListException("User list is empty");
+        }
+    }
+
+    public void clearUserList() {
+        users = new HashMap<>();
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,10 +1,21 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import ru.yandex.practicum.filmorate.model.User;
+
+import javax.swing.text.html.parser.Entity;
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
@@ -13,9 +24,76 @@ public class UserControllerTest {
 
     @Autowired
     private UserController userController;
+    private User user;
+
+    @BeforeEach
+    void beforeEach() {
+        user = new User(1, "test@gmail.com", "testlogin", "Name", LocalDate.of(2000, 1, 1));
+    }
+
+    @AfterEach
+    void afterEach() {
+        userController.clearUserList();
+    }
 
     @Test
-    void addUser_AndExpect200() {
-        User user = new User()
+    void addUser_WithNickAndEmail_AndExpect200() {
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    void addUser_WithoutNickAndWithEmail_AndExpect200() {
+        user.setName("");
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody().getName(), is(user.getLogin()));
+    }
+
+    @Test
+    void addUser_WithoutLogin_AndExpect500() {
+        user.setLogin("");
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @Test
+    void addUser_WithoutMail_AndExpect500() {
+        user.setEmail("");
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @Test
+    void addUser_WithWrongEmail_AndExpect500() {
+        user.setEmail("testgmail.com");
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @Test
+    void updateUser_AndExpect200() {
+        ResponseEntity<User> response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+
+        user.setName("changedName");
+
+        response = restTemplate.postForEntity("/user", user, User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody().getName(), is("changedName"));
+    }
+
+    @Test
+    void getAllUsers_AndExpect200() {
+        restTemplate.postForEntity("/user", user, User.class);
+        ResponseEntity<User[]> response = restTemplate.getForEntity("/users", User[].class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(new User[]{user}));
+    }
+
+    @Test
+    void getAllUsers_EmptyList_AndExpect500() {
+        ResponseEntity<User> response = restTemplate.getForEntity("/users", User.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
     }
 }
