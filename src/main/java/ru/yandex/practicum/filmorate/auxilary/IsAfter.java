e557daa847bf8f0357cc53a56ac5f277a902ab6b
Daniil <rusbear060@gmail.com>
2023-04-13 13:44:06 +0300

    fix style issue

--- src/main/java/ru/yandex/practicum/filmorate/auxilary/IsAfter.java
+++ src/main/java/ru/yandex/practicum/filmorate/auxilary/IsAfter.java
@@ -13,7 +13,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Constraint(validatedBy = DateValidator.class)
 @Documented
-public @interface IsAfter{
+public @interface IsAfter {
     String message() default "{message.key}";
     String current();
     Class<?>[] groups() default {};
