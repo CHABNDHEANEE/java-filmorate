ecbe74a5068458dc18bf2d8239a0c763f206a79d
Daniil <rusbear060@gmail.com>
2023-04-26 15:22:51 +0300

    add ReviewController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
+
+@RequiredArgsConstructor
+@RestController
+@Component
+@RequestMapping("/reviews")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public Review addReview(@RequestBody Review review) {
+        return reviewService.addReview(review);
+    }
+
+    @PutMapping
+    public Review updateReview(@RequestBody Review review) {
+        return reviewService.updateReview(review);
+    }
+
+    @DeleteMapping("/{id}")
+    public Review deleteReviewById(@RequestParam Integer id) {
+        return reviewService.deleteReview(id);
+    }
+
+    @GetMapping("/{id}")
+    public Review getReviewById(@RequestParam Integer id) {
+        return reviewService.getReviewById(id);
+    }
+}
