98b928110a4967473c8d017439ca2de7d6d35cb2
Daniil <rusbear060@gmail.com>
2023-04-26 18:42:55 +0300

    add ReviewLikesDaoImpl

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/ReviewLikesDaoImpl.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.ReviewLikesDao;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewLikesDaoImpl implements ReviewLikesDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void like(int reviewId, int userId) {
+        String sql =
+                "INSERT INTO review_likes (review_id, is_like, user_id) " +
+                        "VALUES (?, ?, ?)";
+
+        jdbcTemplate.update(sql, reviewId, Boolean.TRUE, userId);
+    }
+
+    @Override
+    public void dislike(int reviewId, int userId) {
+        String sql =
+                "INSERT INTO review_likes (review_id, is_like, user_id) " +
+                        "VALUES (?, ?, ?)";
+
+        jdbcTemplate.update(sql, reviewId, Boolean.FALSE, userId);
+
+    }
+
+    @Override
+    public void deleteLike(int reviewId, int userId) {
+        String sql =
+                "DELETE FROM review_likes WHERE review_id = ? AND is_like = ? AND user_id = ?";
+
+        jdbcTemplate.update(sql, reviewId, Boolean.TRUE, userId);
+    }
+
+    @Override
+    public void deleteDislike(int reviewId, int userId) {
+        String sql =
+                "DELETE FROM review_likes WHERE review_id = ? AND is_like = ? AND user_id = ?";
+
+        jdbcTemplate.update(sql, reviewId, Boolean.FALSE, userId);
+    }
+}
