1e028d4f4120db27d0e8c24e4f1b3fb6ec6c66ad
Vladislav <vldslv.sh@gmail.com>
2023-05-01 18:54:53 +0300

    fix: Moving recommendations to the userController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.RecommendationService;
 import ru.yandex.practicum.filmorate.service.UserDbService;
 
 import javax.validation.Valid;
@@ -16,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserDbService userService;
+    private final RecommendationService recommendationService;
 
     @PostMapping("/users")
     public User addUser(@Valid @RequestBody User user) {
@@ -68,4 +71,10 @@ public class UserController {
 
         return userService.getCommonFriends(id, otherId);
     }
+
+    @GetMapping("/users/{id}/recommendations")
+    public List<Film> recommendations(@PathVariable() int id) {
+        return recommendationService.getRecommendation(id);
+    }
+
 }
